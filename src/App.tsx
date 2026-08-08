ce3404a9618288eaf3730471a71c324f5459c7c5
David  Slutsky <david.sl@lsports.eu>
2026-08-08 14:27:53 +0300

    feat: remove "Request a call from David" quick action

--- src/App.tsx
+++ src/App.tsx
@@ -52,11 +52,6 @@ const QUICK_ACTIONS: QuickAction[] = [
     autoSend: false,
   },
   {
-    label: "Request a call from David",
-    text: "Please have David reach out to me. My contact details: ",
-    autoSend: false,
-  },
-  {
     label: "Why David applied to my company",
     text: "Why did David choose to send his CV to my company? Company name: ",
     autoSend: false,
